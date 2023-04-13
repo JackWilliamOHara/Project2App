0cd96d5f6520021371fca95d61c0e1863c025308
JackWilliamOHara <a00286924@student.ait.ie>
2023-04-13 17:13:07 +0100

    test
    test

---
+++ Project22/app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.View;
+
+import static com.example.project2.display.colorList;
+import static com.example.project2.display.current_brush;
+import static com.example.project2.display.pathlist;
+
+public class MainActivity extends AppCompatActivity {
+    public static Path path = new Path();
+    public static Paint paint_brush = new Paint();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void pencil(View view) {
+        paint_brush.setColor(Color.BLACK);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void eraser(View view) {
+        pathlist.clear();
+        colorList.clear();
+        path.reset();
+    }
+
+    public void redColor(View view) {
+        paint_brush.setColor(Color.RED);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void blueColor(View view) {
+        paint_brush.setColor(Color.BLUE);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void greenColor(View view) {
+        paint_brush.setColor(Color.GREEN);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void magentaColor(View view) {
+        paint_brush.setColor(Color.MAGENTA);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void yellowColor(View view) {
+        paint_brush.setColor(Color.YELLOW);
+        currentColor(paint_brush.getColor());
+    }
+    public void currentColor(int c){
+        current_brush = c;
+        path = new Path();
+    }
+}

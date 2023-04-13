a15cd167ccd4697dc793395c9d273bb8e570e7fd
JackWilliamOHara <a00286924@student.ait.ie>
2023-04-13 22:02:21 +0100

    Login (Cian Martin)
    Login system made by Cian Martin

---
+++ Project2login/Project2login/app/src/main/java/com/example/project2_paintingapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.project2_paintingapp;
+
+import static com.example.project2_paintingapp.Display.colorList;
+import static com.example.project2_paintingapp.Display.current_brush;
+import static com.example.project2_paintingapp.Display.pathlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Path path = new Path();
+    public static Paint paint_brush = new Paint();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pencil(View view) {
+        paint_brush.setColor(Color.BLACK);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void eraser(View view) {
+        pathlist.clear();
+        colorList.clear();
+        path.reset();
+    }
+
+    public void redColor(View view) {
+        paint_brush.setColor(Color.RED);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void blueColor(View view) {
+        paint_brush.setColor(Color.BLUE);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void greenColor(View view) {
+        paint_brush.setColor(Color.GREEN);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void yellowColor(View view) {
+        paint_brush.setColor(Color.YELLOW);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void magentaColor(View view) {
+        paint_brush.setColor(Color.MAGENTA);
+        currentColor(paint_brush.getColor());
+    }
+
+    private void currentColor(int c) {
+        current_brush = c;
+        path = new Path();
+    }
+}

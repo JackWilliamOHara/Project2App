71ae2833da02b48a3764e55163209c90eb901b22
JackWilliamOHara <a00286924@student.ait.ie>
2023-05-03 13:29:19 +0100

    Nevest Version
    Multiple canvas created and canvas updated

---
+++ Project2PaintingApp-MultiCanvas/Project2PaintingApp/app/src/main/java/com/example/project2_paintingapp/Home.java
@@ -0,0 +1,55 @@
+package com.example.project2_paintingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Home extends AppCompatActivity {
+
+    FirebaseFirestore firestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String, Object> users = new HashMap<>();
+        users.put("FirstName","Jack");
+        users.put("LastName", "O'Hara");
+        users.put("Description","Hello");
+
+        firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(Home.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Home.this,"Failure", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void goPractice(View view)
+    {
+        Intent i = new Intent(Home.this, Canvas.class);
+        startActivity(i);
+    }
+
+
+}

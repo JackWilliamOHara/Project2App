9864067d45581c7b9e94bc28414f6ff623b88ef1
JackWilliamOHara <a00286924@student.ait.ie>
2023-05-10 14:24:13 +0100

    Linked Canvas

---
+++ Project2PaintingApp-LinkedCanvas/Project2PaintingApp/app/src/main/java/com/example/project2_paintingapp/Join.java
@@ -0,0 +1,35 @@
+package com.example.project2_paintingapp;
+
+import static com.example.project2_paintingapp.R.id.signupbtn;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Join extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+
+
+        EditText username = (EditText) findViewById(R.id.username);
+
+        MaterialButton regbtn = (MaterialButton) findViewById(signupbtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+                Toast.makeText(Join.this,"Username is: "+username1,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ Project2PaintingApp-LinkedCanvas/Project2PaintingApp/app/src/main/java/com/example/project2_paintingapp/Join3.java
@@ -0,0 +1,72 @@
+package com.example.project2_paintingapp;
+
+import static com.example.project2_paintingapp.Display.colorList;
+import static com.example.project2_paintingapp.Display.current_brush;
+import static com.example.project2_paintingapp.Display.pathlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import static com.example.project2_paintingapp.Canvas.paint_brush;
+import static com.example.project2_paintingapp.Canvas.path;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.view.View;
+
+public class Join3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join3);
+    }
+
+    public void pencil(View view) {
+        paint_brush.setColor(Color.BLACK);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void eraser(View view) {
+        paint_brush.setColor(Color.WHITE);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void redColor(View view) {
+        paint_brush.setColor(Color.RED);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void blueColor(View view) {
+        paint_brush.setColor(Color.BLUE);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void greenColor(View view) {
+        paint_brush.setColor(Color.GREEN);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void yellowColor(View view) {
+        paint_brush.setColor(Color.YELLOW);
+        currentColor(paint_brush.getColor());
+    }
+
+    public void magentaColor(View view) {
+        paint_brush.setColor(Color.MAGENTA);
+        currentColor(paint_brush.getColor());
+    }
+
+    private void currentColor(int c) {
+        current_brush = c;
+        path = new Path();
+    }
+
+    public void goNext(View view)
+    {
+        Intent i = new Intent(Join3.this, Home.class);
+        startActivity(i);
+    }
+}
